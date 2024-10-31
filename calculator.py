bddbcc51e3ba54902422691bb27e3d7e4e4c68e6
RueDelmae <enopiamae@gmail.com>
2024-10-31 15:50:42 +0800

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,37 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    pass
+
+def multiply(a, b):
+    pass
+
+def divide(a, b):
+    pass
+
+def main():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    n1 = float(input("-> "))
+    n2 = float(input("-> "))
+
+    if choice == '1':
+        print(f"= {add(n1, n2)}")
+    elif choice == '2':
+        print(f"= {subtract(n1, n2)}")
+    elif choice == '3':
+        print(f"= {multiply(n1, n2)}")
+    elif choice == '4':
+        print(f"= {divide(n1, n2)}")
+    else:
+        print("Invalid input")
+
+if __name__ == "__main__":
+    main()
